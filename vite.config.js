098c65ef08c9ac5c3e89c5ea648eaeab675bc3a9
w3cdpass <killu1875@gmail.com>
2025-05-02 12:56:56 +0530

    vite/project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/CI-CD-practice/',
+  build: {
+    outDir: 'dist',          // Explicit output directory
+    emptyOutDir: true        // Cleans dist folder before build
+  },
   plugins: [react()],
 })
